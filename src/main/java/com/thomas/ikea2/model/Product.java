70839e92b7f4ecab987770a991bb622a374e5d1e
Tomzing <43709104+Tomzing@users.noreply.github.com>
2024-02-17 12:33:23 +0100

    Changed dependencies, added product model

---
+++ src/main/java/com/thomas/ikea2/model/Product.java
@@ -0,0 +1,42 @@
+package com.thomas.ikea2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String description;
+
+    private Integer price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    //Under normal circumstances, should not be used
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
